8f680405cb3a36f64ab88ef2804a3f091ff18fc6
diawando <abduldiawade777@gmail.com>
2023-02-14 23:40:05 +0000

    creation et affichage d'une variable dans app.py et index.html

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,4 +6,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
