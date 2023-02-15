bd1af37884ddfdfc6474fbe443f21a2aee94a496
diawando <abduldiawade777@gmail.com>
2023-02-15 15:57:14 +0000

    ajout de la route comments

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+   
 @app.route('/')
 def index():
     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
@@ -11,3 +11,13 @@ def index():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+@app.route('/comments/')
+def comments():
+    comments = ['This is the first comment. ',
+                'This is the second comment.',
+                'This is the third comment.',
+                'This is the fourth comment.'
+                ]
+    
+    return render_template('comments.html', comments=comments)
